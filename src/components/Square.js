3a8741e1bb3a701fc15d4459c0cd8fd2b8b704db
jricks86 <jricks86@gmail.com>
2023-05-08 14:35:40 -0700

    board created

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Square = (props) => {
   return (
-    <div className="square"></div>
+    <div className="grid">
+    {props.squares.map((value, index) => {
+      return (
+        <div className="square" key={index}></div>
+  
+       
+         
+      )
+    })}
+    </div>
   )
 }
 export default Square
