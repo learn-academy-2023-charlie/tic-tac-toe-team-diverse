669c5f88e0911dd828d59df2b4d94d2b93e38689
wabowen89 <wabowen89@gmail.com>
2023-05-08 15:16:27 -0700

    Add click function to populate X in square as well as alternating X and O for each user

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
+  const [user, setUser] = useState(true)
+
+  const handleClick = (selectedIndex) => {
+    const newSquares = [...squares]
+
+    newSquares[selectedIndex] = user ? "X" : "O"
+    setSquares(newSquares)
+    setUser(!user)
+
+  }
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <Square squares={squares}/>
+      <Square 
+      squares={squares}
+      handleClick = {handleClick}
+      />
     </>
   )
+
 }
 
 export default App
