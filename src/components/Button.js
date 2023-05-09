f28f226a84430a14f97eb8be9b95046f200596ea
wabowen89 <wabowen89@gmail.com>
2023-05-09 11:42:03 -0700

    Corrected syntax error

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = (props) => {
   return (
    <>
-      <button onClcik = {props.handleRestart}> Restart the game!</button>
+      <button onClick = {props.handleRestart}> Restart the game!</button>
    </>
   )
 }
